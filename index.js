70696159efc4e6fe2fc960d35933d73702b98cb4
Mikhail Makharia <makharia.study@gmail.com>
2024-02-19 03:04:59 +0300

    Flavor messages and prettier formatting

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-import rds from 'readline-sync';
-import makeAppendBook from './makebook.js';
-import { bookArray } from './bookarray.js';
-import viewBooks from './printbooks.js';
+import rds from "readline-sync";
+import makeAppendBook from "./makebook.js";
+import viewBooks from "./printbooks.js";
 
 function startCommandLibrary() {
-    let exit = false;
-    console.log("Greetings, curious reader! \n Enter w to write a new book \n Enter v to view our catalogue \n Enter d to delete a book by its name \n Enter c if you want to change an existing book \n Enter q to quit the program");
-    switch(rds.question(': ')) {
-        case "q":
-            exit = true;
-            break;
-        case "w":
-            makeAppendBook(rds.question("Name: "), rds.question("Author: "), rds.question("Genre: "), rds.question("Word count: "), rds.question("Page count: "));
-            break;
-        case "v":
-            viewBooks();
-        default:
-            break;
-    }
-    if (!exit) {
-        startCommandLibrary();
-    }
+  let exit = false;
+  console.log(
+    "Greetings, curious reader! \n Enter w to write a new book \n Enter v to view our catalogue \n Enter d to delete a book by its name \n Enter c if you want to change an existing book \n Enter q to quit the program"
+  );
+  switch (rds.question(": ")) {
+    case "q":
+      exit = true;
+      break;
+    case "w":
+      makeAppendBook(
+        rds.question("Name: "),
+        rds.question("Author: "),
+        rds.question("Genre: "),
+        rds.question("Word count: "),
+        rds.question("Page count: ")
+      );
+      break;
+    case "v":
+      viewBooks();
+    default:
+      break;
+  }
+  if (!exit) {
+    startCommandLibrary();
+  } else {
+    console.log('See you next time!');
+  }
 }
 
 startCommandLibrary();
--- makebook.js
+++ makebook.js
@@ -1,29 +1,35 @@
-import rds from 'readline-sync';
-import crypto from 'crypto';
-import { bookArray, appendBookArray } from './bookarray.js';
-import { readFileSync, writeFileSync } from 'fs';
-
-export default function makeAppendBook(name = "", author = "", genre = "", wordcount = 0, pagecount = 0) {
-    const encoder = `${name}${author}${wordcount}`
-    const hash = crypto.createHmac('sha256', encoder).update(genre).digest('hex');
-    const bookObject = {
-        "name": name,
-        "author": author,
-        "genre": genre,
-        "wordcount": +wordcount,
-        "pagecount": +pagecount,
-        "ID": hash
+import rds from "readline-sync";
+import crypto from "crypto";
+import { appendBookArray } from "./bookarray.js";
+export default function makeAppendBook(
+  name = "",
+  author = "",
+  genre = "",
+  wordcount = 0,
+  pagecount = 0
+) {
+  const encoder = `${name}${author}${wordcount}`;
+  const hash = crypto.createHmac("sha256", encoder).update(genre).digest("hex");
+  const bookObject = {
+    name: name,
+    author: author,
+    genre: genre,
+    wordcount: +wordcount,
+    pagecount: +pagecount,
+    ID: hash,
+  };
+  console.log(bookObject);
+  for (let i = 0; i < 3; i += 1) {
+    const usAnswer = rds.question(
+      "Is this correct? (You can change the book entry later) - y/n: "
+    );
+    if (usAnswer === "y") {
+      appendBookArray(bookObject);
+      return;
+    } else if (usAnswer === "n") {
+      return;
     }
-    console.log(bookObject);
-    for (let i = 0; i += 1; i < 2) {
-        const usAnswer = rds.question('Is this correct? (You can change the book entry later) - y/n: ')
-        if ( usAnswer === 'y') {
-            appendBookArray(bookObject);
-            return;
-        } else if ( usAnswer === 'n') {
-            return;
-        }
-    }
-    console.log('Aborting...');
-    return;
+  }
+  console.log("Aborting...");
+  return;
 }
